1e7b33affb93741d5c5b49226b03284307227086
shawnya <shawnaya@workspace.com>
2023-02-12 18:21:52 -0600

    disparate code added

--- disparate/disparate.cc
+++ disparate/disparate.cc
@@ -6,11 +6,9 @@ using namespace std;
 int main ()
 {
   {
-    /////////////////////////////////
-    // CREATE THREE int32_t VALUES //
-    // (big, small, sum)           //
-    /////////////////////////////////
-
+    int32_t big =1e8;
+    int32_t small = 1;
+    int32_t sum = big + small; 
 
     cout << "int32_t:" << endl;
     cout << "  big   = " << big   << endl;
@@ -19,11 +17,10 @@ int main ()
   }
 
   {
-    ///////////////////////////////
-    // CREATE THREE float VALUES //
-    // (big, small, sum)         //
-    ///////////////////////////////
-
+  
+      float big =1e8;
+      float small= 1;
+      float sum = big + small;
 
     cout << "float:" << endl;
     cout << "  big   = " << fixed << setprecision(1) << big   << endl;
