76a34c20e1b34062b73c590d13666439c7ded9e8
shawnya <shawnaya@workspace.com>
2023-02-12 18:16:47 -0600

    added code

--- overflow_underflow/overflow_underflow.cc
+++ overflow_underflow/overflow_underflow.cc
@@ -5,21 +5,15 @@ using namespace std;
 int main ()
 {
   {
-    ///////////////////////////////////
-    // CREATE SIGNED,UNSIGNED 32-BIT //
-    // INTEGERS NAMED s,u AND        //
-    // INITIALIZE THEM TO ZERO       //
-    ///////////////////////////////////
-
+    signed int32_t s = 0;
+    unsigned int32_t u = 0;
 
     cout << "32-bit before decrement:" << endl;
     cout << "  signed:   " << s << endl;
     cout << "  unsigned: " << u << endl;
 
-    ///////////////////
-    // DECREMENT s,u //
-    ///////////////////
-
+    --s;
+    --u;
 
     cout << "32-bit after decrement:" << endl;
     cout << "  signed:   " << s << endl;
@@ -27,21 +21,15 @@ int main ()
   }
 
   {
-    ///////////////////////////////////
-    // CREATE SIGNED,UNSIGNED 16-BIT //
-    // INTEGERS NAMED s,u AND        //
-    // INITIALIZE THEM TO MAXIMUMS   //
-    ///////////////////////////////////
-
+        int16_t s = 32767;
+      uint16_t u = 65535;
 
     cout << "16-bit before increment:" << endl;
     cout << "  signed:   " << s << endl;
     cout << "  unsigned: " << u << endl;
 
-    ////////////////////////
-    // INCREMENT s,u HERE //
-    ////////////////////////
-
+      ++s;
+      ++u;
 
     cout << "16-bit after increment:" << endl;
     cout << "  signed:   " << s << endl;
